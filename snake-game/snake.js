f939f8d60dac14ef21f3730089decba20a00c332
Kate Catlin <kcatlin2@gmail.com>
2017-05-06 13:09:18 -0600

    Arrow keys work

--- snake-game/snake.js
+++ snake-game/snake.js
@@ -1,23 +1,29 @@
-// var snake = [{ top: 0, left: 0}];
-// var drawableSnake = { color: "pink", pixels: snake };
-// var drawableObjects = [drawableSnake];
-// CHUNK.draw(drawableObjects);
-
 var drawSnake = function(snakeToDraw) {
   var drawableSnake = { color: "green", pixels: snakeToDraw };
   var drawableObjects = [drawableSnake];
   CHUNK.draw(drawableObjects);
 }
 
+var moveSegment = function(segment) {
+  if (segment.direction === "down") {
+    return { top: segment.top + 1, left: segment.left }
+  } else if (segment.direction === "up") {
+    return { top: segment.top - 1, left: segment.left }
+  } else if (segment.direction === "right") {
+    return { top: segment.top, left: segment.left + 1 }
+  } else if (segment.direction === "left") {
+    return { top: segment.top, left: segment.left - 1 }
+  }
+  return segment;
+}
+
 var moveSnake = function(snake) {
   var oldSegment = snake[0];
-  var newSegment = { top: oldSegment.top, left: oldSegment.left +1 };
+  var newSegment = moveSegment(oldSegment);
+  newSegment.direction = oldSegment.direction;
   var newSnake = [newSegment];
   return newSnake;
 }
-
-// var snake = [{ top: 0, left: 0}];
-// drawSnake(snake);
 
 
 var advanceGame = function() {
@@ -25,5 +31,10 @@ var advanceGame = function() {
   drawSnake(snake);
 }
 
-var snake = [{ top: 0, left: 0}];
+var changeDirection = function(direction) {
+  snake[0].direction = direction;
+}
+
+var snake = [{ top: 0, left: 0, direction: "down" }];
 CHUNK.executeNTimesPerSecond(advanceGame, 1);
+CHUNK.onArrowKey(changeDirection);
