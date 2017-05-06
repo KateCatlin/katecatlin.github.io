b09787daf37547564c0ccf64de68a23d07be529f
Kate Catlin <kcatlin2@gmail.com>
2017-05-06 12:11:07 -0600

    Adding moveSnake function

--- snake-game/snake.js
+++ snake-game/snake.js
@@ -9,5 +9,12 @@ var drawSnake = function(snakeToDraw) {
   CHUNK.draw(drawableObjects);
 }
 
+var moveSnake = function(snake) {
+  var oldSegment = snake[0];
+  var newSegment = { top: oldSegment.top + 1, left: oldSegment.left };
+  var newSnake = [newSegment];
+  return newSnake;
+}
+
 var snake = [{ top: 0, left: 0}];
 drawSnake(snake);
